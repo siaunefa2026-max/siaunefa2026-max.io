bfbdbd838cc42b4dd5f33efd9bba9d1831fac9e3
siaunefa2026-max <siaunefa2026@gmail.com>
2026-02-10 20:48:54 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,9 @@
 const SB_URL = "https://lsrcjgwaaztpqubrusfa.supabase.co";
-const SB_KEY = "XGQ1Xg7sO52uOfmo";
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzcmNqZ3dhYXp0cHF1YnJ1c2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NjY2MzgsImV4cCI6MjA4NjM0MjYzOH0.1wQzD7Ki2qy5Rt9muLL7fev_o0VyI67QDdEhguNbEIk";
 const _supabase = supabase.createClient(SB_URL, SB_KEY);
 
 // Exportamos una función para obtener el cliente
 function getClient() {
     return _supabase;
+
 }
